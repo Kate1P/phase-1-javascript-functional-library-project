b6f7752ae7fefb62769a490bfcc002123bc800af
Kate Popova <popova1ea@gmail.com>
2023-01-01 23:15:29 -0800

    commit tree cf28a821d5dff67c8bc1912f7ec2f83d37a20475

---
+++ index.js
@@ -0,0 +1,34 @@
+
+function myEach(collection, alert){
+let arr = Object.values(collection)
+arr.forEach(element => alert(element))
+return collection
+}
+
+function myMap(collection, callback){
+    let arr = Object.values(collection)
+    let newArr = []
+    for (let element of arr){
+       newArr.push(callback(element))
+    }
+    return newArr
+}
+
+function myReduce(collection, callback, ...acc) {
+    let arr = Object.values(collection)
+    let newAcc;
+    let i;
+    let total = 0;
+    if (acc.length > 0) {
+        newAcc = acc[0]
+        i=0 
+    } else{
+        newAcc = arr[0]
+        i=1
+    }
+    for (i; i <arr.length; i++){
+        newAcc = callback(newAcc,arr[i], collection)
+
+    }
+return newAcc
+}
